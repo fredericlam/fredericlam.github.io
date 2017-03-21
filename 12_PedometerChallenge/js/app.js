0991304aa5f9035bf1d45d2a38253b28d9fdf7a4
LAM Frédéric <flaaam@gmail.com>
2017-03-21 09:31:30 +0100

    multi pie chart

--- 12_PedometerChallenge/js/app.js
+++ 12_PedometerChallenge/js/app.js
@@ -1,7 +1,7 @@
 
 
 	var source = [] , dataset = [] , teamsets = [], players = [] , daily = [] , weekly = [] , users = [] ; 
-	var y , x ; 
+	var y , x , tooltip ; 
 
 	var myDay = new Date();
 	var today = { 'day' : myDay.getUTCDate() , 'month' : myDay.getMonth() + 1  }
@@ -73,7 +73,7 @@
 	   	}
 
 
-	   	$('#daily_stat').html('Statistics on '+(today.day-1)+'/0'+today.month) ;
+	   	$('#daily_stat').html('Statistics the '+(today.day-1)+'/0'+today.month) ;
 
 	   	$('a.day_stat').click( function(){
 
@@ -88,7 +88,7 @@
 
 	   		var d = dataset.key.split('-') ; 
 
-	   		$('#daily_stat').html('Statistics on '+(Math.abs(d[0])+1)+'/0'+d[1]) ;
+	   		$('#daily_stat').html('Statistics the '+(Math.abs(d[0])+1)+'/0'+d[1]) ;
 
 	   		// top 10 team
 	   		var daily_team = d3.nest()
@@ -195,7 +195,7 @@
 			// d3.layout.pie to compute the angles for each arc. These start and end angles
 			// are passed to d3.svg.arc to draw arcs! Note that the arc radius is specified
 			// on the arc, not the layout.
-			svg.selectAll("path")
+			svg_pies.selectAll("path")
 			    .data(d3.layout.pie().value(function(d){ return d.step ; }))
 			  	.enter().append("path")
 			  	.attr('class','pie')
@@ -303,7 +303,7 @@
 				.key(function(d){ return d.date_entry ; })
 				.entries( dataset ) ; 
 
-			$('a.day_stat[attr-day="'+numDayToday+'"]').trigger('click');
+			$('a.day_stat[attr-day="1"]').trigger('click');
 
 			$("#total_steps span").prop('Counter', 0 ).animate({
 				Counter: sum
@@ -351,11 +351,36 @@
 			x.domain([0, d3.max(teamsets, function(d) { return d.values.total; })]);
 			y.domain(teamsets.map(function(d,i) { return rank(i)+d.key; }));
 
+			tooltip = d3.select('body')            
+	          .append('div')                             
+	          .attr('class', 'canTooltip');                 
+	        ;  
+
 			var bar = svg.selectAll("g.bar")
-			  .data(teamsets)
-			.enter().append("g")
-			  .attr("class", "bar")
-			  .attr("transform", function(d,i) { return "translate(0," + y(rank(i)+d.key) + ")"; });
+			  	.data(teamsets)
+			  	.enter().append("g")
+			  	.attr("class", "bar")
+			  	.attr("transform", function(d,i) { return "translate(0," + y(rank(i)+d.key) + ")"; })
+			  	.on("mousemove", function(d){
+
+	                tooltip.style('display', 'block');
+
+	                var coords = d3.mouse(this);
+
+	                tooltip
+	                	.style('top', (coords[1] - 60 ) + 'px')
+	                	.style('left', (coords[0] - 80 ) + 'px');
+
+		        })
+		        // mouseout function            
+		        .on("mouseout", function(d){
+
+		            document.body.style.cursor = 'auto';
+		            tooltip.style('display','none');
+		            d3.select(this).style('fill-opacity',1) ; 
+
+		        })    
+			;
 
 			bar.append("rect")
 			  .attr("width", function(d) { return x(d.values.total); })
@@ -376,73 +401,9 @@
 
 			svg.append("g")
 			  .attr("class", "y axis")
-			  //.attr("transform","translate(-150,0)")
-			  .call(yAxis)
-			  /*.selectAll("text")
-    		  .attr("x", 6)
-              .style("text-anchor", "start")*/
-			;
+			  .call(yAxis);
 
 			// ranking / progression 
-			// set the dimensions and margins of the graph
-			var margin = {top: 20, right: 20, bottom: 30, left: 50},
-			    width = 960 - margin.left - margin.right,
-			    height = 500 - margin.top - margin.bottom;
-
-			// parse the date / time
-			var parseTime = d3.timeParse("%d-%b-%y");
-
-			// set the ranges
-			var x = d3.scaleTime().range([0, width]);
-			var y = d3.scaleLinear().range([height, 0]);
-
-			// define the line
-			var valueline = d3.line()
-			    .x(function(d) { return x(d.date); })
-			    .y(function(d) { return y(d.close); });
-
-			// append the svg obgect to the body of the page
-			// appends a 'group' element to 'svg'
-			// moves the 'group' element to the top left margin
-			var svg = d3.select("body ").append("svg")
-			    .attr("width", width + margin.left + margin.right)
-			    .attr("height", height + margin.top + margin.bottom)
-			  .append("g")
-			    .attr("transform",
-			          "translate(" + margin.left + "," + margin.top + ")");
-
-			// Get the data
-			d3.csv("data.csv", function(error, data) {
-			  if (error) throw error;
-
-			  // format the data
-			  data.forEach(function(d) {
-			      d.date = parseTime(d.date);
-			      d.close = +d.close;
-			  });
-
-			  // Scale the range of the data
-			  x.domain(d3.extent(data, function(d) { return d.date; }));
-			  y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-			  // Add the valueline path.
-			  svg.append("path")
-			      .data([data])
-			      .attr("class", "line")
-			      .attr("d", valueline);
-
-			  // Add the X Axis
-			  svg.append("g")
-			      .attr("transform", "translate(0," + height + ")")
-			      .call(d3.axisBottom(x));
-
-			  // Add the Y Axis
-			  svg.append("g")
-			      .call(d3.axisLeft(y));
-
-			});
-
-
 		});
 
 	}) ; 
